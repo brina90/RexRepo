2fe61cb2501ece5766d721fe40d4e05115b815d6
brina90 <orjiwisdom51@gmail.com>
2026-01-27 19:31:17 +0100

    save

---
+++ suit website/script.js
@@ -0,0 +1,5 @@
+const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+    menuIcon.addEventListener("click", () => {
+      menu.classList.toggle("show");
+    });
